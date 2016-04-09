0095ff968d773abedf0a1d78d1b583d916aba08b
Jason Corriveau <jpc028@bucknell.edu>
2016-04-08 22:05:10 -0400

    Basic start to pokemon class and move class

---
+++ src/PokemonObjects/Pokemon.java
@@ -0,0 +1,24 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2016
+ *
+ * Name: Benjamin Matase, Jason Corriveau, Eric Marshall, Alexander Murph
+ * Date: Apr 8, 2016
+ * Time: 5:34:25 PM
+ *
+ * Project: csci205FinalProject
+ * Package: PokemonObjects
+ * File: Pokemon
+ * Description:
+ *
+ * ****************************************
+ */
+package PokemonObjects;
+
+/**
+ *
+ * @author Jason
+ */
+public class Pokemon {
+
+}
