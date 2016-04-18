ee7766cb75720361d11a0ffc6eef63d43160f857
Murph <acm026@bucknell.edu>
2016-04-18 10:00:57 -0400

    Documenting and controller fix

--- src/BattleUtility/BattleCalculator.java
+++ src/BattleUtility/BattleCalculator.java
@@ -9,7 +9,8 @@
  * Project: csci205FinalProject
  * Package: BattleUtility
  * File: BattleCalculator
- * Description:
+ * Description: Class used to calculate all damage for a single attack
+ * using stats of attacking and defending pokemon.
  *
  * ****************************************
  */
@@ -20,11 +21,19 @@ import PokemonObjects.Move.AttackType;
 import PokemonObjects.Pokemon;
 
 /**
+ * Abstract of Damage Calculations in Pokemon
  *
  * @author Murph
  */
 public class BattleCalculator {
 
+    /**
+     * Type chart based off the Pokemon type advantages. Ordered in the same way
+     * as our enum to check how two types interact.
+     *
+     * @see <http://pokemondb.net/type>
+     * @author Murph
+     */
     static double[][] TypeChart
                       = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0, 1, 1, 0.5},
                          {1, 0.5, 0.5, 1, 2, 2, 1, 1, 1, 1, 1, 2, 0.5, 1, 0.5, 1, 2},
@@ -51,6 +60,15 @@ public class BattleCalculator {
     private double criticalModifier;
     private double accuracyModifier;
 
+    /**
+     * Takes in Attacking and Defending Pokemon, and the move used and sets them
+     * as attributes for calculations later.
+     *
+     * @param AtkPoke Pokemon
+     * @param DefPoke Pokemon
+     * @param move Move
+     * @author Murph
+     */
     public BattleCalculator(Pokemon AtkPoke, Pokemon DefPoke, Move move) {
         this.AtkPoke = AtkPoke;
         this.DefPoke = DefPoke;
@@ -59,10 +77,23 @@ public class BattleCalculator {
         this.accuracyModifier = getAccuracyModifier();
     }
 
+    /**
+     * Allows outside sources to set a new move to the same calculator. Added to
+     * make things easier for the AI Utility.
+     *
+     * @param move
+     * @author Murph
+     */
     public void setMove(Move move) {
         this.move = move;
     }
 
+    /**
+     * Called on a damage calculator object to gather the amount of damage.
+     *
+     * @return damage double
+     * @author Murph
+     */
     public double damageCalculator() {
         AttackType attackType = move.getType();
         double damage;
@@ -75,6 +106,12 @@ public class BattleCalculator {
         return damage;
     }
 
+    /**
+     * Used when calculating a physical damage move.
+     *
+     * @return damage double
+     * @author Murph
+     */
     private double physicalDamageCalculator() {
         double modifier = getModifier1();
         double modifier2;
@@ -93,6 +130,12 @@ public class BattleCalculator {
         return damage;
     }
 
+    /**
+     * Used when calculating a special damage move.
+     *
+     * @return damage double
+     * @author Murph
+     */
     private double specialDamageCalculator() {
         double modifier = getModifier1();
         double modifier2;
@@ -111,6 +154,12 @@ public class BattleCalculator {
         return damage;
     }
 
+    /**
+     * Returns all the strings that the GUI should display, in order.
+     *
+     * @return String
+     * @author Murph
+     */
     public String getOutcome() {
         String response = null;
         double modifier = getModifier1();
@@ -138,6 +187,12 @@ public class BattleCalculator {
         return response;
     }
 
+    /**
+     * Used by the AI to quickly find the damage modifiers of a given move.
+     *
+     * @return modifier double
+     * @author Murph
+     */
     public double AIMoveAdvantage() {
         double modifier = getModifier1();
         double modifier2;
@@ -148,6 +203,12 @@ public class BattleCalculator {
         return modifier;
     }
 
+    /**
+     * Gets the first modifier (i.e looks at primary type of defending Pokemon)
+     *
+     * @return modifier double
+     * @author Murph
+     */
     private double getModifier1() {
         PokemonType moveType = move.getDamageType();
         PokemonType defType = DefPoke.getPokemonType1();
@@ -155,6 +216,13 @@ public class BattleCalculator {
         return modifier;
     }
 
+    /**
+     * Gets the first modifier (i.e looks at secondary type of defending
+     * Pokemon)
+     *
+     * @return modifier double
+     * @author Murph
+     */
     private double getModifier2() {
         PokemonType moveType = move.getDamageType();
         PokemonType defType = DefPoke.getPokemonType2();
@@ -162,6 +230,12 @@ public class BattleCalculator {
         return modifier;
     }
 
+    /**
+     * Gets the STAB for an attack (Same Type Attack Bonus)
+     *
+     * @return modifier double
+     * @author Murph
+     */
     private double getStab() {
         double stab = 1.0;
         if (AtkPoke.getPokemonType1() == move.getDamageType()
@@ -171,6 +245,13 @@ public class BattleCalculator {
         return stab;
     }
 
+    /**
+     * Handles the randomization for critical hits, and returns the relevant
+     * modifier.
+     *
+     * @return modifier double
+     * @author Murph
+     */
     private double getCriticalModifier() {
         double random = Math.random();
         double critMod = 1.0;
@@ -180,6 +261,13 @@ public class BattleCalculator {
         return critMod;
     }
 
+    /**
+     * Calculates Accuracy statistic. Causes move to deal no damage if
+     * randomizatino causes a miss, otherwise has no effect.
+     *
+     * @return modifier double
+     * @author Murph
+     */
     private double getAccuracyModifier() {
         double random = Math.random();
         double accMod = 1.0;
